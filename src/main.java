005fd3fe0bb228b847edd07c464861d5a0e5b508
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-16 13:19:37 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 package main; //not compleatly sure if this is needed
 
 import utils.greet; // Import the class from the utils package
-import utils.greetFormal; //imports class form folder
+import utils.info.greetFormal; //imports class form folder
 
 public class Main {
     public static void main(String[] args) {
