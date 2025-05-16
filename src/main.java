9580794f5f4215b2bf9f30f9dfb7021b0c94b160
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-16 13:11:19 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,16 @@
+package main; //not compleatly sure if this is needed
+
+import utils.greet; // Import the class from the utils package
+
+public class Main {
+    public static void main(String[] args) {
+        // Call
+        Utils.greet();  // Calls the static method
+        
+
+        // // Create an instance of MyHelper if it has non-static methods
+        // MyHelper helper = new MyHelper();
+        // helper.someMethod();
+    }
+}
 
